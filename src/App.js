473d91207b2ec018db35963365916357efc16127
oriolgriera <oriolgrierajimenez@gmail.com>
2021-10-13 16:04:18 +0200

    Upload react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to react-app App</h1>
+        <p>This is my first app with React</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
